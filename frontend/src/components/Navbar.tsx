0dc63a682fa5ffaf2672aec364e96cc6b45c5b2c
adithya-santhosh <adithyasanthosh4@gmail.com>
2026-05-26 21:07:12 +0530

    Initial code For Navbar

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,3 +1,16 @@
 export default function Navbar() {
-    return <nav>Navbar</nav>;
+    return (
+      <nav className="flex justify-between items-center p-4 border-b">
+        <h1 className="text-2xl font-bold">MerchStore</h1>
+        <div className="flex gap-6">
+          <a href="/">Home</a>
+          <a href="/products">Products</a>
+          <a href="/contact">Contact</a>
+          <a href="/login">Login/User</a>
+          
+
+        </div>
+        
+      </nav>
+    );
   }
