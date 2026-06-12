8dd8d2d67b964bc16b5e994909282bdd7e79763d
adithya-santhosh <adithyasanthosh4@gmail.com>
2026-06-12 15:10:09 +0530

    Refactor server configuration to use environment variable for port
    - Updated server.ts to define the server port using an environment variable, defaulting to 5000.
    - Modified the server startup log to include the port number for better clarity.

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import productRoutes from "./routes/product.routes";
 
+const PORT = process.env.PORT || 5000
 
 const app = express();
 
@@ -17,7 +18,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/products", productRoutes);
 
-app.listen(5000, () => {
-  console.log("Server running");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
